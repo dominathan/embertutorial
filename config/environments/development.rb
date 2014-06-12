e4e31f8ae7dfd7cf07b8fc01f19a00c1041e3a64
Nathan Hall <nathan.mh@gmail.com>
2014-06-12 13:10:24 -0500

    added to environment file which ember version to use

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  #which ember.js to use
+  config.ember.variant  = :development
+
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,4 +36,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  #which version ember version to use for logging
+  config.ember.variant = :development
 end
